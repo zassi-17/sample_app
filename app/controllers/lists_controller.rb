8343929dd5192630cb14c8476d8ea2d8c0e1fbb5
Ubuntu <ubuntu@ip-172-31-25-78.ap-northeast-1.compute.internal>
2025-08-09 10:13:37 +0000

    [finish]3-7

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -6,7 +6,7 @@ class ListsController < ApplicationController
   def create
     list = List.new(list_params)
     list.save
-    redirect_to'/top'
+    redirect_to list_path(list.id)
   end
 
   def index
@@ -14,13 +14,27 @@ class ListsController < ApplicationController
   end
 
   def show
+    @list = List.find(params[:id])
   end
 
   def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
+  end
+
+  def destroy
+    list = List.find(params[:id])
+    list.destroy
+    redirect_to '/lists'
   end
 
   private
   def list_params
-    params.require(:list).permit(:title, :body)
+    params.require(:list).permit(:title, :body, :image)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  get 'lists/new'
-  post 'lists' => 'lists#create'
-  get 'lists/show'
-  get 'lists/edit'
+
   get '/top' => 'homes#top'
-  get 'lists' => 'lists#index'
+  resources :lists
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
